39f4145d0d7739991e0f25a65260a54d905e05c0
thethiny <thethiny@ymail.com>
2026-01-08 19:24:40 +0400

    Map Names

--- constants.tsx
+++ constants.tsx
@@ -10,51 +10,51 @@ export const LINE_COLORS: Record<LineID, string> = {
 
 const stations: Station[] = [
   
-  { id: 'G1', name: 'G1', line: ['green'], x: 40, y: 120, isTransfer: false },
-  { id: 'G2', name: 'G2', line: ['green'], x: 330, y: 10, isTransfer: false },
-  { id: 'G3', name: 'G3', line: ['green'], x: 570, y: 40, isTransfer: false },
-  { id: 'GB1', name: 'GB1', line: ['green', 'blue'], x: 680, y: 210, isTransfer: true },
-  { id: 'G4', name: 'G4', line: ['green'], x: 710, y: 280, isTransfer: false },
-  { id: 'GR1', name: 'GR1', line: ['green', 'red'], x: 610, y: 420, isTransfer: true },
-  { id: 'G5', name: 'G5', line: ['green'], x: 450, y: 390, isTransfer: false },
-  { id: 'G6', name: 'G6', line: ['green'], x: 320, y: 260, isTransfer: false },
-  { id: 'GR2', name: 'GR2', line: ['green', 'red'], x: 240, y: 260, isTransfer: true },
-  { id: 'GB2', name: 'GB2', line: ['green', 'blue'], x: 80, y: 230, isTransfer: true },
+  { id: 'G1', name: 'ТОПЕНЕ', line: ['green'], x: 40, y: 120, isTransfer: false },
+  { id: 'G2', name: 'ПОЛЕН', line: ['green'], x: 330, y: 10, isTransfer: false },
+  { id: 'G3', name: 'ЛИВАДА', line: ['green'], x: 570, y: 40, isTransfer: false },
+  { id: 'GB1', name: 'НАВОДНЕНИЯ', line: ['green', 'blue'], x: 680, y: 210, isTransfer: true },
+  { id: 'G4', name: 'ДЪГА', line: ['green'], x: 710, y: 280, isTransfer: false },
+  { id: 'GR1', name: 'ПРЕЗРЯЛО', line: ['green', 'red'], x: 610, y: 420, isTransfer: true },
+  { id: 'G5', name: 'ПАПЕ', line: ['green'], x: 450, y: 390, isTransfer: false },
+  { id: 'G6', name: 'ПРЯСНО', line: ['green'], x: 320, y: 260, isTransfer: false },
+  { id: 'GR2', name: 'ВОДОРАСЛИ', line: ['green', 'red'], x: 240, y: 260, isTransfer: true },
+  { id: 'GB2', name: 'ДЖОГИНГ', line: ['green', 'blue'], x: 80, y: 230, isTransfer: true },
 
-  { id: 'B1', name: 'B1', line: ['blue'], x: 210, y: 110, isTransfer: false },
-  { id: 'B2', name: 'B2', line: ['blue'], x: 450, y: 80, isTransfer: false },
-  { id: 'B3', name: 'B3', line: ['blue'], x: 750, y: 120, isTransfer: false },
+  { id: 'B1', name: 'ВЪЛНА', line: ['blue'], x: 210, y: 110, isTransfer: false },
+  { id: 'B2', name: 'ШЕЙНА', line: ['blue'], x: 450, y: 80, isTransfer: false },
+  { id: 'B3', name: 'ТОПЕНЕ', line: ['blue'], x: 750, y: 120, isTransfer: false },
   // GB1 here
-  { id: 'B4', name: 'B4', line: ['blue'], x: 560, y: 210, isTransfer: false },
-  { id: 'BY1', name: 'BY1', line: ['blue', 'yellow'], x: 480, y: 290, isTransfer: true },
-  { id: 'B5', name: 'B5', line: ['blue'], x: 450, y: 320, isTransfer: false },
-  { id: 'B6', name: 'B6', line: ['blue'], x: 350, y: 420, isTransfer: false },
-  { id: 'BY2', name: 'BY2', line: ['blue', 'yellow'], x: 130, y: 370, isTransfer: true },
-  { id: 'B7', name: 'B7', line: ['blue'], x: 80, y: 320, isTransfer: false },
+  { id: 'B4', name: 'КАМИНА', line: ['blue'], x: 560, y: 210, isTransfer: false },
+  { id: 'BY1', name: 'ШПИФЕР', line: ['blue', 'yellow'], x: 480, y: 290, isTransfer: true },
+  { id: 'B5', name: 'СКИ', line: ['blue'], x: 450, y: 320, isTransfer: false },
+  { id: 'B6', name: 'ПОЛЮС', line: ['blue'], x: 350, y: 420, isTransfer: false },
+  { id: 'BY2', name: 'РАЗЛАГАНЕ', line: ['blue', 'yellow'], x: 130, y: 370, isTransfer: true },
+  { id: 'B7', name: 'ВИЕЛИЦА', line: ['blue'], x: 80, y: 320, isTransfer: false },
   // GB2 Here
 
-  { id: 'R1', name: 'R1', line: ['red'], x: 390, y: 130, isTransfer: false },
-  { id: 'R2', name: 'R2', line: ['red'], x: 590, y: 130, isTransfer: false },
-  { id: 'R3', name: 'R3', line: ['red'], x: 810, y: 270, isTransfer: false },
+  { id: 'R1', name: 'ТОПЛО', line: ['red'], x: 390, y: 130, isTransfer: false },
+  { id: 'R2', name: 'СЯНКА', line: ['red'], x: 590, y: 130, isTransfer: false },
+  { id: 'R3', name: 'ЖЕГА', line: ['red'], x: 810, y: 270, isTransfer: false },
   // GR1 Here
-  { id: 'R4', name: 'R4', line: ['red'], x: 590, y: 540, isTransfer: false },
-  { id: 'R5', name: 'R5', line: ['red'], x: 370, y: 560, isTransfer: false },
-  { id: 'RY1', name: 'RY1', line: ['red', 'yellow'], x: 280, y: 520, isTransfer: true },
-  { id: 'R6', name: 'R6', line: ['red'], x: 240, y: 370, isTransfer: false },
+  { id: 'R4', name: 'СЛЪНЦЕ', line: ['red'], x: 590, y: 540, isTransfer: false },
+  { id: 'R5', name: 'САНДАЛИ', line: ['red'], x: 370, y: 560, isTransfer: false },
+  { id: 'RY1', name: 'СВЕТУЛКА', line: ['red', 'yellow'], x: 280, y: 520, isTransfer: true },
+  { id: 'R6', name: 'БАСЕЙН', line: ['red'], x: 240, y: 370, isTransfer: false },
   // GR2 Here
-  { id: 'RY2', name: 'RY2', line: ['red', 'yellow'], x: 270, y: 200, isTransfer: true },
+  { id: 'RY2', name: 'СЛЪНЧОГЛЕД', line: ['red', 'yellow'], x: 270, y: 200, isTransfer: true },
 
   // BY2 Here
-  { id: 'Y1', name: 'Y1', line: ['yellow'], x: 190, y: 230, isTransfer: false },
+  { id: 'Y1', name: 'КАНЕЛА', line: ['yellow'], x: 190, y: 230, isTransfer: false },
   // RY2 Here
-  { id: 'Y2', name: 'Y2', line: ['yellow'], x: 390, y: 200, isTransfer: false },
+  { id: 'Y2', name: 'ОРАНЖЕВ', line: ['yellow'], x: 390, y: 200, isTransfer: false },
   // BY1 Here
-  { id: 'Y3', name: 'Y3', line: ['yellow'], x: 530, y: 340, isTransfer: false },
-  { id: 'Y4', name: 'Y4', line: ['yellow'], x: 530, y: 490, isTransfer: false },
-  { id: 'Y5', name: 'Y5', line: ['yellow'], x: 390, y: 520, isTransfer: false },
+  { id: 'Y3', name: 'ШУМА', line: ['yellow'], x: 530, y: 340, isTransfer: false },
+  { id: 'Y4', name: 'ПОРОЙ', line: ['yellow'], x: 530, y: 490, isTransfer: false },
+  { id: 'Y5', name: 'ЖИТО', line: ['yellow'], x: 390, y: 520, isTransfer: false },
   // RY1 Here
-  { id: 'Y6', name: 'Y6', line: ['yellow'], x: 140, y: 520, isTransfer: false },
-  { id: 'Y7', name: 'Y7', line: ['yellow'], x: 30, y: 490, isTransfer: false },
+  { id: 'Y6', name: 'КРАТУНА', line: ['yellow'], x: 140, y: 520, isTransfer: false },
+  { id: 'Y7', name: 'РЕКОЛТА', line: ['yellow'], x: 30, y: 490, isTransfer: false },
 ];
 
 export const STATIONS = stations;
